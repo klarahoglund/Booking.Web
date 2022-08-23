38abef50b4ca23b6c17512e02342ccd7af97f9ad
Klara Höglund <klarahoglund@hotmail.com>
2022-08-23 17:30:17 +0200

    Scaffolding igen

---
+++ Booking.Data/ApplicationContextFactory.cs
@@ -0,0 +1,25 @@
+﻿
+using Booking.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Data
+{
+
+    //För att koppla mellan kontext i olika projekt om det behövs scafflodas tex
+    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+     
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+            options.UseSqlServer("aspnet-Booking.Web-4BA2A107-E930-45AE-A2DB-C82B817B2F24");
+            return new ApplicationDbContext(options.Options);
+        }
+    }
+}
